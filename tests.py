5de8a5ee7fee0dade42e4a60dbd8e41f8158e14d
unknown <>
2016-02-01 08:28:34 +0900

    TC 개선 : Supply Object 가 task_no(과제 번호)로 조회 하는 것이 자연스럽지 못함

--- tests.py
+++ tests.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 import unittest
-__author__ = 'yooyoung-mo'
-
-
-class TaskSupplyTest(unittest.TestCase):
-
+   
+  
+class TaskTest(unittest.TestCase):
+      
     # 과제 목록 공급표 구하기
-    def get_test_task_supply(self):
-        supply = Supply()
-
-        task_supply = supply.get(task_no='0000')
-        self.assertEqual(task_supply.task_no, '0000')
-        self.assertEqual(task_supply.customer, u'에스사')
-        self.assertEqual(task_supply.abbreviations, u'정보계')
-        self.assertEqual(task_supply.contractor, u'앤사> 당사')
-        self.assertEqual(task_supply.abbreviations, u'정보계')
-        self.assertEqual(task_supply.contract_date, '14/02/01~15/03/20')
-        self.assertEqual(task_supply.amount_of_order, 100)
-        self.assertEqual(task_supply.this_year, 46)
-        self.assertEqual(task_supply.next_year, 0)
-        self.assertEqual(task_supply.three_years_from_now, 0)
-        self.assertEqual(task_supply.participants, u'김아무 김아무')
-        self.assertEqual(task_supply.category1, u'분1')
-        self.assertEqual(task_supply.category2, u'')
-        self.assertEqual(task_supply.category3, u'')
-        self.assertEqual(task_supply.representative, u'이아무')
+    def test_get_supply(self):
+        task = Task(task_no='0000')
+         
+        supply = task.get_supply()
+        self.assertEqual(supply.customer, u'에스사')
+        self.assertEqual(supply.abbreviations, u'정보계')
+        self.assertEqual(supply.contractor, u'앤사> 당사')
+        self.assertEqual(supply.abbreviations, u'정보계')
+        self.assertEqual(supply.contract_date, '14/02/01~15/03/20')
+        self.assertEqual(supply.amount_of_order, 100)
+        self.assertEqual(supply.this_year, 46)
+        self.assertEqual(supply.next_year, 0)
+        self.assertEqual(supply.three_years_from_now, 0)
+        self.assertEqual(supply.participants, u'김아무 김아무')
+        self.assertEqual(supply.category1, u'분1')
+        self.assertEqual(supply.category2, u'')
+        self.assertEqual(supply.category3, u'')
+        self.assertEqual(supply.representative, u'이아무')
+          
+          
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
