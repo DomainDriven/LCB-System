1aa11c462163eba9751a8623309ce6254bfda6be
unknown <>
2016-02-01 08:30:14 +0900

    TC 개선 : Supply 데이터 구조를 좀더 명확하게 구조화

--- tests.py
+++ tests.py
@@ -6,22 +6,24 @@ class TaskTest(unittest.TestCase):
       
     # 과제 목록 공급표 구하기
     def test_get_supply(self):
-        task = Task(task_no='0000')
+        task = Task.get(task_no='0000')
+        self.assertEqual(task.abbreviations, u'정보계')
          
-        supply = task.get_supply()
-        self.assertEqual(supply.customer, u'에스사')
-        self.assertEqual(supply.abbreviations, u'정보계')
-        self.assertEqual(supply.contractor, u'앤사> 당사')
-        self.assertEqual(supply.abbreviations, u'정보계')
-        self.assertEqual(supply.contract_date, '14/02/01~15/03/20')
+        supply = task.get_supply()   
+        self.assertEqual(supply.customer.name, u'에스사')
+        self.assertEqual(supply.contract.owner, u'앤사')
+        self.assertEqual(supply.contract.contractor, u'당사')
+        self.assertEqual(supply.contract.start_date, '14/02/01')
+        self.assertEqual(supply.contract.end_date, '15/03/20')
         self.assertEqual(supply.amount_of_order, 100)
         self.assertEqual(supply.this_year, 46)
         self.assertEqual(supply.next_year, 0)
         self.assertEqual(supply.three_years_from_now, 0)
-        self.assertEqual(supply.participants, u'김아무 김아무')
-        self.assertEqual(supply.category1, u'분1')
-        self.assertEqual(supply.category2, u'')
-        self.assertEqual(supply.category3, u'')
+        self.assertEqual(supply.participants[0], u'김아무')
+        self.assertEqual(supply.participants[2], u'김아무')
+        self.assertEqual(supply.categories[0], u'분1')
+        self.assertEqual(supply.categories[1], u'')
+        self.assertEqual(supply.categories[2], u'')
         self.assertEqual(supply.representative, u'이아무')
           
           
