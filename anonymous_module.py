176c0e33854c514a4c31e6f542d7d45a0e368676
unknown <>
2016-02-04 10:32:44 +0900

    PEP8 적용

--- anonymous_module.py
+++ anonymous_module.py
@@ -8,28 +8,30 @@ Created on 2016. 2. 4.
 
 def get_dummy_data():
     '''
-         계획은 비휘발성 자장소(DB 혹은 File)에서 데이터를 조회 해야 하나
-         현재는 메모리 상에서 임시 데이터를 반환 한다. 
+    계획은 비휘발성 자장소(DB 혹은 File)에서 데이터를 조회 해야 하나
+    현재는 메모리 상에서 임시 데이터를 반환 한다.
     '''
     return {
-            'task_no' : '0000',
-            'task_summary' : {
-                               'customer' : u'에스사',
-                               'abbreviations' : u'정보계',
-                               'order_company' : u'엔사',
-                               'representative' : u'이아무',
-                               'contract_start_date' : '2014-02-01',
-                               'contract_end_date' : '2015-03-20',
-                               'amount_of_order' : 100000000,
-                               'amount_of_order_type' : 'SI',    
-                               'in_personnel' : ['김아무', '김아무'],
-                               'category1' : '분1',
-                               'category2' : '',
-                               'category3' : '',                    
+            'task_no': '0000',
+            'task_summary': {
+                               'customer': u'에스사',
+                               'abbreviations': u'정보계',
+                               'order_company': u'엔사',
+                               'representative': u'이아무',
+                               'contract_start_date': '2014-02-01',
+                               'contract_end_date': '2015-03-20',
+                               'amount_of_order': 100000000,
+                               'amount_of_order_type': 'SI',
+                               'in_personnel': ['김아무', '김아무'],
+                               'category1': '분1',
+                               'category2': '',
+                               'category3': '',
                               },
-            'sales' : {
-                       '2015' : 46359176,
-                       '2016' : 0,
-                       '2017' : 0,
+            'sales': {
+                       '2015': 46359176,
+                       '2016': 0,
+                       '2017': 0,
                        },
             }
+
+# TO-DO : 테스트 케이스를 통과만 하는 최소의 동작 하는 코드 작성 한다.
--- tests.py
+++ tests.py
@@ -1,41 +1,30 @@
 # -*- coding: utf-8 -*-
 import unittest
-   
-  
+
+
 class TaskTest(unittest.TestCase):
-      
     # 과제 목록 공급표 구하기
     def test_get_supply(self):
         task = Task.get(task_no='0000')
         self.assertEqual(task.abbreviations, u'정보계')
-         
-        supply = task.get_supply()   
+        # 공급
+        supply = task.get_supply()
         self.assertEqual(supply.customer.name, u'에스사')
         self.assertEqual(supply.contract.owner, u'앤사')
         self.assertEqual(supply.contract.contractor, u'당사')
         self.assertEqual(supply.contract.start_date, '14/02/01')
         self.assertEqual(supply.contract.end_date, '15/03/20')
         self.assertEqual(supply.amount_of_order, 100)
-        
         # 매출
-        self.assertEqual(supply.sales.this_year, 46) # 올해 
-        self.assertEqual(supply.sales.next_year, 0)# 내년
-        self.assertEqual(supply.sales.three_years_from_now, 0) # 내후년
-        
-        
+        self.assertEqual(supply.sales.this_year, 46)
+        self.assertEqual(supply.sales.next_year, 0)
+        self.assertEqual(supply.sales.three_years_from_now, 0)
         # 과제 참여자
         self.assertEqual(supply.participants.length(), 2)
         self.assertEqual(supply.participants[0], u'김아무')
         self.assertEqual(supply.participants[1], u'김아무')
-        
         # 분류
         self.assertEqual(supply.category.main, u'분1')
         self.assertEqual(supply.category.middle, u'')
         self.assertEqual(supply.category.sub, u'')
-        
         self.assertEqual(supply.representative, u'이아무')
-          
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
