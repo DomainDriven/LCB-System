f6f8aaed3a142df76160cbeaf275af2c3e02f52a
unknown <>
2016-02-12 09:47:45 +0900

    엑셀 파일 양식에 따라 가짜 데이터 구조를 재 구성

--- anonymous_module.py
+++ anonymous_module.py
@@ -11,31 +11,39 @@ def get_dummy_data():
     계획은 비휘발성 자장소(DB 혹은 File)에서 데이터를 조회 해야 하나
     현재는 메모리 상에서 임시 데이터를 반환 한다.
     """
-    return {
-            'task_no': '0000',
-            'task_summary': {
-                               'customer': u'에스사',
-                               'abbreviations': u'정보계',
-                               'order_company': u'엔사',
-                               'representative': u'이아무',
-                               'contract_start_date': '2014-02-01',
-                               'contract_end_date': '2015-03-20',
-                               'amount_of_order': 100000000,
-                               'amount_of_order_type': 'SI',
-                               'in_personnel': ['김아무', '김아무'],
-                               'category1': '분1',
-                               'category2': '',
-                               'category3': '',
-                              },
-            'sales': {
-                       '2015': 46359176,
-                       '2016': 0,
-                       '2017': 0,
-                       },
-            }
+    return [
+        {
+            'project_id': 0000,
+            'project_summary': {
+                'customer':  u'에스사',
+                'abbreviations': u'정보계',
+                'contract_name': u'정보계 개선',
+                'order_company': u'앤사',
+                'order_company_representative': u'이아무',
+                'amount_of_order': 100000000,
+                'amount_of_order_type': 'SI',
+                'contract_start_date': '2014-02-01',
+                'contract_end_date': '2015-03-20',
+                'commitment_start_date': '2014-02-01',
+                'commitment_start_date': '2015-03-20',
+                'commitment_resource': [
+                    '김아무', '김아무'
+                ],
+                'category1':'분1',
+                'category2':'',
+                'category3':'',
+            },
+            'sales_buy': {
+                '2014': 149640833,
+                '2015': 46359176,
+                '2016': 0,
+                '2017': 0,
+            },
+        },
+    ]
 
 
-class Task:
+class Project:
     """
     '과제'를
     """
--- tests.py
+++ tests.py
@@ -6,24 +6,23 @@ class Test(unittest.TestCase):
     # 과제 공급표 구하기
     def test_get_supply(self):
         # given. when
-        # # 과제 조회 by 과제 번호
-        project = Project.get(project_no='0000')
-        # 조회한 과제의 공급표 조회
+        # # 공급표 조회(by 과제 번호)
+        project = Project(project_no='0000')
         project_supply = project.get_supply()
 
         # then
         # # 과제 번호
-        self.assertEqual(project.project_id, '0000')
+        self.assertEqual(project_supply.project_id, '0000')
         # # 고객(갑)
-        self.assertEqual(project_supply.customer.name, u'에스사')
+        self.assertEqual(project_supply.customer, u'에스사')
         # # 과제 약칭
-        self.assertEqual(project_supply.abbreviations, u'정보계')
+        self.assertEqual(project_supply.project_abbreviations, u'정보계')
         # # 을_병_정_무
-        self.assertEqual(project_supply.contract.owner, u'앤사')
-        self.assertEqual(project_supply.contract.contractor, u'당사')
+        self.assertEqual(project_supply.fulfillment_company[0], u'앤사')
+        self.assertEqual(project_supply.fulfillment_company[1], u'당사')
         # # 계약 기한
-        self.assertEqual(project_supply.contract.start_date, '14/02/01')
-        self.assertEqual(project_supply.contract.end_date, '15/03/20')
+        self.assertEqual(project_supply.contract_start_date, '14/02/01')
+        self.assertEqual(project_supply.contract_end_date, '15/03/20')
         # # 수주액
         self.assertEqual(project_supply.amount_of_order, 100)
         # # 매출(2015, 2016, 2017)
@@ -35,7 +34,7 @@ class Test(unittest.TestCase):
         self.assertEqual(project_supply.participants[1], u'김아무')
         # # 분류1,2,3
         self.assertEqual(project_supply.categories[0], u'분1')
-        self.assertEqual(project_supply.categories[0], u'')
+        self.assertEqual(project_supply.categories[1], u'')
         self.assertEqual(project_supply.categories[2], u'')
         # # 대표자
         self.assertEqual(project_supply.representative, u'이아무')
