74f4038071efcdf275335d10cf9fe1e400573fe4
Youngmo Yoo <gigamadness@gmail.com>
2016-02-19 09:51:17 +0900

    더미 데이터를 Django ORM으로 변환

--- supply/models.py
+++ supply/models.py
@@ -1,5 +1,42 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    수행 과제
+    """
+    project_no = models.CharField(max_length=10, primary_key=True)
+    customer = models.CharField(max_length=100)
+    abbreviations = models.CharField(max_length=100)
+    contract_name = models.CharField(max_length=100)
+    contract_start_date = models.DateField()
+    contract_end_date = models.DateField()
+    order_company = models.CharField(max_length=100)
+    order_company_representative = models.CharField(max_length=100)
+    amount_of_order = models.BigIntegerField()
+    amount_of_order_type = models.CharField(max_length=45)
+    category1 = models.CharField(max_length=45)
+    category2 = models.CharField(max_length=45)
+    category3 = models.CharField(max_length=45)
+
+
+class Employee(models.Model):
+    """
+    직원
+    """
+    employee_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project)
+
+
+class SalesBuy(models.Model):
+    """
+    매출입/과총
+    """
+    sales_id = models.AutoField(primary_key=True)
+    year = models.CharField(max_length=4)
+    amount = models.BigIntegerField()
+    project = models.ForeignKey(Project)
--- supply/tests.py
+++ supply/tests.py
@@ -1,3 +1,77 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from supply.models import Project, Employee, SalesBuy
+
+
+class ProjectTest(TestCase):
+    def test_save_and_retrieve(self):
+        # given
+        project = Project.objects.create(project_no='0000',
+                                         customer=u'에스사',
+                                         abbreviations=u'정보계',
+                                         contract_name=u'정보계 개선',
+                                         contract_start_date=datetime(year=2014, month=2, day=1),
+                                         contract_end_date=datetime(year=2015, month=3, day=20),
+                                         order_company=u'앤사',
+                                         order_company_representative=u'이아무',
+                                         amount_of_order=100000000,
+                                         amount_of_order_type='SI',
+                                         category1=u'분1')
+        # when
+        actual = Project.objects.get(project_no=project.project_no)
+
+        # then
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(project, actual)
+
+
+class ProjectAndEmployee(TestCase):
+    def test_save_and_retrieve(self):
+        # given
+        project = Project.objects.create(project_no='0000',
+                                         customer=u'에스사',
+                                         abbreviations=u'정보계',
+                                         contract_name=u'정보계 개선',
+                                         contract_start_date=datetime(year=2014, month=2, day=1),
+                                         contract_end_date=datetime(year=2015, month=3, day=20),
+                                         order_company=u'앤사',
+                                         order_company_representative=u'이아무',
+                                         amount_of_order=100000000,
+                                         amount_of_order_type='SI',
+                                         category1=u'분1')
+        Employee.objects.create(name=u'김아무', project=project)
+        Employee.objects.create(name=u'김아무2', project=project)
+
+        # when
+        actual_employees = Employee.objects.filter(project__project_no=project.project_no)
+
+        # then
+        self.assertEqual(len(actual_employees), 2)
+
+
+class ProjectAndSalesBuy(TestCase):
+    def test_save_and_retrieve(self):
+        # given
+        project = Project.objects.create(project_no='0000',
+                                         customer=u'에스사',
+                                         abbreviations=u'정보계',
+                                         contract_name=u'정보계 개선',
+                                         contract_start_date=datetime(year=2014, month=2, day=1),
+                                         contract_end_date=datetime(year=2015, month=3, day=20),
+                                         order_company=u'앤사',
+                                         order_company_representative=u'이아무',
+                                         amount_of_order=100000000,
+                                         amount_of_order_type='SI',
+                                         category1=u'분1')
+
+        SalesBuy.objects.create(year='2014', amount=149640833, project=project)
+        SalesBuy.objects.create(year='2015', amount=46359176, project=project)
+
+        # when
+        actual_sales_buy = SalesBuy.objects.filter(project__project_no=project.project_no)
+
+        # then
+        self.assertEqual(len(actual_sales_buy), 2)
