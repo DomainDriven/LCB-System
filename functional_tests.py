57b225a191be657283864f077f6219dd8e1095e9
Youngmo Yoo <gigamadness@gmail.com>
2016-02-22 09:56:50 +0900

    실패하는 기능 테스트 케이스 작성

--- functional_tests.py
+++ functional_tests.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
-from django.test import LiveServerTestCase
+import unittest
+
+from selenium import webdriver
 
 
-class ProjectSupplyTest(LiveServerTestCase):
+class ProjectSupplyTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path="webdriver\\chromedriver")
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
     def test_retrieve_supply(self):
         """
         수행 과제 공급표 조회
         """
         # Beck은 수행 과제에 대한 공급표를 조회 하기 위해 '예실 대비' 웹 사이트를 방문 한다.
+        self.browser.get('http://localhost:8000')
+
         # 웹 사이트는 수행 과제 번호를 입력할 수 있는 입력 상자와 공급표를 조회할 수 있는 버튼을 재공 하고 있다.
+        input_project_no = self.browser.find_element_by_id('project_no')
+        button_retrieve_project_supply = self.browser.find_element_by_id('retrieve_project_supply')
+
+        self.assertIsNone(input_project_no)
+        self.assertIsNotNone(button_retrieve_project_supply)
+
         # Beck은 수행 과제 번호(0000)를 입력한다
         # 그리고 Beck은  '공급표 조회' 버튼을 누른다.
         # 수행 과제 번호에 해당하는 공급표가 보인다.
